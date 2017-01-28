aceeeaf44b1e57b04e803d2214914a7c6bc74e19
Kirthika S <kirthika05@gmail.com>
2017-01-27 21:58:45 -0600

    Initial commit

---
+++ src/com/kirthika/Main.java
@@ -0,0 +1,30 @@
+package com.kirthika;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            if(a>=2 && isPrime(a)){
+                System.out.println("Prime");
+            }else{
+                System.out.println("Not prime");
+            }
+        }
+        sc.close();
+    }
+
+    public static boolean isPrime(int n){
+        for(int i=2;i<=Math.sqrt(n);i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
